05afe40e6483e88c349b07b98635abd309a2e57d
gr-okazaki <masanori.okazaki@gmo-research.jp>
2016-11-14 14:12:18 +0900

    no message

--- Bot.php
+++ Bot.php
@@ -9,11 +9,12 @@ $client = new Slack\RealTimeClient($loop);
 $client->setToken(BotConfig::TOKEN);
 
 $client->on('message', function ($data) use ($client) {
-    echo "Someone typed a message: ".$data['text']. " : " . $data['channel'] . "\n";
+    echo "Someone typed a message: " . $data['text'] . " : " . $data['channel'] . "\n";
 
-    $client->getChannelById($data['channel'])->then(function ($chanel) use ($client) {
-        $client->send('TESTTEST', $chanel);
-    });
+    $client->getChannelById($data['channel'])
+        ->then(function ($chanel) use ($client) {
+            $client->send('TESTTEST', $chanel);
+        });
 });
 
 $client->connect()->then(function () {
