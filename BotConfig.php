5bda8077606f0e590021d708ff8c9d66a62044a9
okakam <non@okakam.net>
2016-11-19 18:55:08 +0900

    Delete BotConfig.php

--- BotConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class BotConfig {
-    const SLACK_TOKEN = 'xoxb-102671332017-S0wzIg8yKcf2Bdi4hxD0AKb2';
-    const SLACK_BOT_HASH = 'U30KR9S0H';
-
-    const CHAT_TOKEN = 'e1bb6a10b1aa6b23b8dc';
-}
