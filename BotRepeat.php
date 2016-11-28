4356016ac507f302cb10549c958b013abd379e7e
non <non@okakam.net>
2016-11-28 19:44:38 +0900

    オーム返しBOT

---
+++ BotRepeat.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+require_once 'BotConfig.php';
+date_default_timezone_set('Asia/Tokyo');
+
+$loop = React\EventLoop\Factory::create();
+
+$client = new Slack\RealTimeClient($loop);
+$client->setToken(BotConfig::SLACK_TOKEN_TEST);
+
+$client->on('message', function ($data) use ($client) {
+    print_r("Someone typed a message: " . $data['text'] . " : " . $data['channel'] . "\n");
+
+    $client->getChannelById($data['channel'])->then(function ($chanel) use ($client, $data) {
+        $client->send($data['text'], $chanel);
+    });
+});
+
+$client->connect()->then(function () {
+    print_r('Connect !' . "\n");
+});
+
+$loop->run();
--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-# PHP Starter Overview
+# Slack Chat Sample
 
-The PHP Starter demonstrates a simple, reusable PHP web application.
+PHP + Slack で Botを作ってみよう
 
-## Run the app locally
-
-1. Download and extract [PHP][]
-2. Add the extracted directory to your PATH environment variable
-3. Download and extract the starter code from the Bluemix UI
-4. cd into the app directory
-5. Run `php -S localhost:8000` to start the app using the built-in development web server
-6. Access the running app in a browser at http://localhost:8000
-
-[PHP]: http://php.net/downloads.php
+Userlocal のチャットBotApi を使用して 雑談をします。
